eee903f4c19afd40db7303e0b0eeab4e22fe33e6
daneongrand <danektolstov@gmail.com>
2021-12-01 18:51:47 +0400

    Add cors config into index

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const errorMiddleware = require('./middlewares/errorMiddleware')
 
 const app = express()
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}))
 app.use(express.json())
 app.use('/api', router)
 app.use(errorMiddleware)
